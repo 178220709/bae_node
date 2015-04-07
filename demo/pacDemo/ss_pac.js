85c2b26d36c69e19c3f194ef80bc4b2c6aa9fd67
jsonsong <jian.song@china.zhaogang.com>
2015-04-07 16:08:12 +0800

    fix

---
+++ demo/pacDemo/ss_pac.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2015/4/7.
+ */
+
+
+
+
+
+
+var FindProxyForURL = function(init, profiles) {
+    return function(url, host) {
+        "use strict";
+        var result = init, scheme = url.substr(0, url.indexOf(":"));
+        do {
+            result = profiles[result];
+            if (typeof result === "function") result = result(url, host, scheme);
+        } while (typeof result !== "string" || result.charCodeAt(0) === 43);
+        return result;
+    };
+}("+SS socks", {
+    "+SS socks": function(url, host, scheme) {
+        "use strict";
+        if (host === "[::1]" || host === "localhost" || host === "127.0.0.1") return "DIRECT";
+
+
+
+        return "SOCKS5 127.0.0.1:1080";
+    }
+});
