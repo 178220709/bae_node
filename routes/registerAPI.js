004edb866bb4d201846af950de58e89d820080d5
jsonsong <jian.song@china.zhaogang.com>
2015-04-17 14:22:00 +0800

    fix

--- routes/registerAPI.js
+++ routes/registerAPI.js
@@ -2,40 +2,42 @@
 /*global global, require, process, module, baejs*/
 /*jslint node: true */
 
-
 var express = require('express');
 var router = express.Router();
-var _ =  require('underscore');
+var _ = require('underscore');
 
 //注册api里面的接口
 var apis = ["spider"];
 
 var apiList = [];//item : ctrl , action , fun
-_.each(apis,function(apiName){
-  var _api = require("../api/"+apiName+".js");
+_.each(apis, function (apiName) {
+    var _api = require("../api/" + apiName + ".js");
 
-  for( var action  in _api ){
-    apiList.push({
-      ctrl:apiName,
-      action:action,
-      fun:_api[action]
+    for (var action  in _api) {
+        apiList.push({
+            ctrl: apiName,
+            action: action,
+            fun: _api[action]
+        });
+    }
+});
+
+_.each(apiList, function (actionObj) {
+    router.post(_.template("/<%= ctrl %>/<%= action %>")(actionObj), actionObj.fun);
+});
+
+
+if (!process.env.USER_ShowApi || process.env.USER_ShowApi === 1) {//配置，关掉api说明
+    router.get('/', function (req, res, next) {
+        res.render('apiShow', {
+            title: 'api 说明', actions: _.map(apiList, function (item) {
+                return {
+                    ctrl: item.ctrl, action: item.action
+                };
+            })
+        });
     });
-  }
-});
-
-_.each(apiList,function(actionObj){
-  router.post( _.template("/<%= ctrl %>/<%= action %>",actionObj) , actionObj.fun );
-});
-
-
-
-if(!(process.env.USER_ShowApi && process.env.USER_ShowApi=="0")){//配置，关掉api说明
-  router.get('/', function(req, res, next) {
-    res.render('apiShow', { title: 'api 说明' , actions: _.map(apiList,function(item){return {ctrl:item.ctrl,action:item.action}})});
-  });
 }
-
-
 
 
 module.exports = router;
