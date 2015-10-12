3b796135254d72b33378636b67c2456deaf783ae
jsonsong <sj178220709@163.com>
2015-10-12 17:35:22 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express2',time: util.formatDate( new Date(),"yyyy-MM-dd: HH:mm:ss") });
+  res.render('index', { title: 'Express3',time: util.formatDate( new Date(),"yyyy-MM-dd: HH:mm:ss") });
 });
 
 module.exports = router;
